c9f0dfed77b4eb1bb7583b649ff1438f5ef1cef1
mihara <mihara@geek.co.jp>
2015-12-21 23:59:20 +0900

    03のステップ1まで

---
+++ php/keisan.php
@@ -0,0 +1,10 @@
+<?php
+	echo "<div style='color: red; font-size: 3em;'>";
+	echo "1 + 2 * 3 =";
+	echo 1 + 2 * 3 ;
+	echo "</div>";
+	echo "<div style='color: blue; font-size: 3em;'>";
+	echo "(1 + 2) * 3 =";
+	echo (1 + 2) * 3 ;
+	echo "</div>";
+?>
---
+++ php/show_var.php
@@ -0,0 +1,15 @@
+<?php
+// 変数に代入する
+$name = "カリナ";
+$age = "20";
+$hobby = 'ギター';
+$img = "calina.jpg";
+// htmlに記述する
+echo "<html><body>\n";
+echo "<ul>\n";
+echo "<li>名前は: {$name}</li>\n";
+echo "<li>年齢は: {$age}</li>\n";
+echo "<li>趣味は: {$hobby}</li>\n";
+echo "</ul>\n";
+echo "</body></html>";
+?>
